608b466888bc60f9e6a64791cf4948db64c7d2a4
pblsnchz <pbl.snchz@gmail.com>
2020-04-05 14:51:00 +0200

    chg: sanity checks

--- src/rtl/console.c
+++ src/rtl/console.c
@@ -678,13 +678,17 @@ HB_FUNC( HB_GETSTDERR ) /* Return handle for STDERR */
 
 HB_FUNC( HB_SETSTDOUT ) /* Set handle for STDOUT */
 {
-   s_hFilenoStdout := ( HB_FHANDLE ) hb_parnint( 1 );
+   if( HB_ISNUM( 1 ) )
+      s_hFilenoStdout = hb_numToHandle( hb_parnint( 1 ) );
+
    hb_ret();
 }
 
 HB_FUNC( HB_SETSTDERR ) /* Set handle for STDERR */
 {
-   s_hFilenoStderr := ( HB_FHANDLE ) hb_parnint( 1 );
+   if( HB_ISNUM( 1 ) )
+      s_hFilenoStderr = hb_numToHandle( hb_parnint( 1 ) );
+
    hb_ret();
 }
 
